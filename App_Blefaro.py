3fd933d3774793470627830561de5e4a199f5f34
João Luiz Sotini <92888122+SotiniJoao@users.noreply.github.com>
2022-07-29 22:35:22 -0300

    Update App_Blefaro.py

--- App_Blefaro.py
+++ App_Blefaro.py
@@ -43,8 +43,14 @@ def open_main_sheets():
     db = pd.DataFrame(db.get_all_records())
     return db, sh
 
+class DBConnect:
+    def __init__(self, sh):
+        self.sh = sh
+    def hash_sh_reference(sh_ref):
+        sh = sh_ref.sh
+        return sh
 
-@st.cache(allow_output_mutation=True, max_entries=5, hash_func={dbconnect:id})
+@st.cache(allow_output_mutation=True, max_entries=5, hash_func={DBConnect:hash_sh_reference})
 def open_personal_db(sh, ids):
     personal_db_raw = open_credencials(sh, ids)
     personal_db = pd.DataFrame(personal_db_raw.get_all_records())
